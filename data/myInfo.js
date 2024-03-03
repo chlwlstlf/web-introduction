962761f46c00c234ba002c3c0ad63264b4475b9c
jinsil <jinsil378900@gmail.com>
2024-03-03 21:18:22 +0900

    feat: 방명록 작성하기

--- data/myInfo.js
+++ data/myInfo.js
@@ -1,7 +1,7 @@
 const myInfo = {
-  name: "텐텐(최진실)",
-  residence: "경기도 오산시",
-  introduction: "안녕하세요. 10월 10일생 텐텐입니다.",
+  이름: "텐텐(최진실)",
+  "사는 곳": "경기도 오산시",
+  "한 줄 소개": "안녕하세요. 10월 10일생 텐텐입니다.",
 };
 
 export default myInfo;
--- js/index.js
+++ js/index.js
@@ -1,16 +1,97 @@
+import myInfo from "../data/myInfo.js";
 import movies from "../data/movies.js";
 
+const $myInfoContent = document.querySelector(".my-info-content");
 const $tableBody = document.querySelector(".table-body");
+const $guestBookCheckBox = document.querySelector(".guest-book-check-box");
+const $submitBtn = document.querySelector(".submit-btn");
+const $guestBookList = document.querySelector(".guest-book-list");
 
-console.log(movies);
-$tableBody.innerHTML = movies
-  .map((movie, index) => {
-    return `<tr>
-      <td>${index + 1}</td>
-      <td>${movie.title}</td>
-      <td><img src="${movie.poster}" alt="${movie.title}"/></td>
-      <td>${movie.plot}</td>
-      <td><a href="${movie.link}">클릭</a></td>
-    </tr>`;
+$myInfoContent.innerHTML = Object.entries(myInfo)
+  .map(([key, value]) => {
+    return `<li>${key}: ${value}</li>`;
   })
   .join("");
+
+$tableBody.innerHTML = movies
+  .map((movie, index) => {
+    return `
+      <tr>
+        <td>${index + 1}</td>
+        <td>${movie.title}</td>
+        <td><img src="${movie.poster}" alt="${movie.title}"/></td>
+        <td>${movie.plot}</td>
+        <td><a href="${movie.link}">클릭</a></td>
+      </tr>
+    `;
+  })
+  .join("");
+
+$guestBookCheckBox.innerHTML = movies
+  .map((movie, index) => {
+    return `            
+      <input
+        type="checkbox"
+        id="${movie.title}"
+        name="${movie.title}"
+        value="${movie.title}"
+        required
+      />
+      <label for="${movie.title}">${movie.title}</label>
+    `;
+  })
+  .join("");
+
+$submitBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const $nameInput = document.querySelector("#name");
+  const $checkedboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+
+  const name = $nameInput.value;
+  const checkedOptions = Array.from($checkedboxes).map((checkbox) => checkbox.value);
+
+  console.log("Name:", name);
+  console.log("Checked options:", checkedOptions);
+  alert(`${name}님, 저와 ${checkedOptions.length}개의 취향이 같으시네요!`);
+  addEntry(name, checkedOptions);
+  showGuestbook();
+  inputClear();
+});
+
+const inputClear = () => {
+  const $nameInput = document.querySelector("#name");
+  const $checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+  $nameInput.value = "";
+  $checkboxes.forEach((checkbox) => {
+    checkbox.checked = false;
+  });
+};
+
+// 방명록 항목 추가
+const addEntry = (name, options) => {
+  const entries = JSON.parse(localStorage.getItem("guestbook")) || [];
+
+  const entry = { name, options };
+
+  entries.push(entry);
+
+  localStorage.setItem("guestbook", JSON.stringify(entries));
+};
+
+// 방명록 업데이트
+const showGuestbook = () => {
+  const entries = JSON.parse(localStorage.getItem("guestbook")) || [];
+  $guestBookList.innerHTML = entries
+    .map((entry, index) => {
+      return `  
+        <div>${index + 1}빠</div>          
+        <div>${entry.name}</div>
+        <div>${entry.options}</div>
+      `;
+    })
+    .join("");
+};
+
+showGuestbook();
