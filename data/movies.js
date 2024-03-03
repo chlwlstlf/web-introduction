df184245421e740ea3ed637c17ac0701e9b07509
jinsil <jinsil378900@gmail.com>
2024-03-03 18:22:50 +0900

    feat: 영화 정보 객체 만들기

---
+++ data/movies.js
@@ -0,0 +1,34 @@
+const movies = [
+  {
+    title: "늑대소년",
+    poster: "../images/movie_1.jpg",
+    plot: "분노하면 늑대로 변하는 주인공과 그를 사랑하는 여주인공 사이의 로맨틱 판타지 이야기를 그린 영화",
+    link: "https://search.naver.com/search.naver?ssc=tab.nx.all&where=nexearch&sm=tab_jum&query=%EB%8A%91%EB%8C%80%EC%86%8C%EB%85%84",
+  },
+  {
+    title: "메이즈 러너1",
+    poster: "../images/movie_2.jpg",
+    plot: "기억을 잃은 소년들이 미로 속에서 생존을 건 모험을 그린 영화",
+    link: "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&ssc=tab.nx.all&query=%EB%A9%94%EC%9D%B4%EC%A6%88%EB%9F%AC%EB%84%88+1&oquery=%EB%8A%91%EB%8C%80%EC%86%8C%EB%85%84&tqi=iPn%2F2dqo1SossvZajeCssssss7h-079568",
+  },
+  {
+    title: "신과 함께",
+    poster: "../images/movie_3.jpg",
+    plot: "죽은 자들의 여정을 그리며, 삶과 죽음의 의미를 탐구하는 감동적인 영화",
+    link: "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&ssc=tab.nx.all&query=%EC%8B%A0%EA%B3%BC%ED%95%A8%EA%BB%98&oquery=%EB%A9%94%EC%9D%B4%EC%A6%88%EB%9F%AC%EB%84%88+1&tqi=iPdsUdqo1SCsstPjsfNssssstsZ-497288",
+  },
+  {
+    title: "알라딘",
+    poster: "../images/movie_4.jpg",
+    plot: "가난한 소년 알라딘이 마법의 램프와 함께 모험을 떠나는 디즈니 애니메이션 실사 영화",
+    link: "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&ssc=tab.nx.all&query=%EC%95%8C%EB%9D%BC%EB%94%98+%EC%98%81%ED%99%94&oquery=%EC%95%8C%EB%9D%BC%EB%94%98&tqi=iPdtLlqo1LwssMZ5Xu4ssssst48-022304",
+  },
+  {
+    title: "나의 소녀시대",
+    poster: "../images/movie_5.jpg",
+    plot: "서로의 첫사랑을 이어주려다가 눈이 맞았지만 고백을 끝내하지 못 한 청춘 영화",
+    link: "https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&ssc=tab.nx.all&query=%EB%82%98%EC%9D%98+%EC%86%8C%EB%85%80%EC%8B%9C%EB%8C%80&oquery=%EC%95%8C%EB%9D%BC%EB%94%98+%EC%98%81%ED%99%94+%EC%86%8C%EA%B0%9C&tqi=iPdt%2Fsqo1e8ssMr95RNssssst%2FZ-131921",
+  },
+];
+
+export default movies;
